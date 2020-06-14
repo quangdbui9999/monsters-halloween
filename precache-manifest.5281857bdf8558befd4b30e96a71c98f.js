a6ce5826c99eaaed01168f9d2b610ca7b12dc893
Quang Bui <quangdbui9999.gmail.com>
2020-06-13 23:30:08 -0400

    Updates

---
+++ precache-manifest.5281857bdf8558befd4b30e96a71c98f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2506995c9f2bb714108d53fa5f4ce1a6",
+    "url": "/monsters-halloween/index.html"
+  },
+  {
+    "revision": "427fb3bc786be3e00f22",
+    "url": "/monsters-halloween/static/css/main.dd5a2a62.chunk.css"
+  },
+  {
+    "revision": "a118fcff70fde5bdac91",
+    "url": "/monsters-halloween/static/js/2.7bede9e7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-halloween/static/js/2.7bede9e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "427fb3bc786be3e00f22",
+    "url": "/monsters-halloween/static/js/main.490f5b02.chunk.js"
+  },
+  {
+    "revision": "9ee99108f3163ad5394e",
+    "url": "/monsters-halloween/static/js/runtime-main.e389fbbc.js"
+  }
+]);
